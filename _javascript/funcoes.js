fdec0b44260d4cb1312f722c015de52a7f95b24c
Lennon Cunha <lenokp21@hotmail.com>
2018-01-03 16:08:34 -0200

    commit css e javascript

---
+++ _javascript/funcoes.js
@@ -0,0 +1,11 @@
+/* endereco do icones do menu */
+var home = "https://raw.githubusercontent.com/lenokp21/projeto-glass-html5/master/_imagens/home.png";
+var oculos = "https://raw.githubusercontent.com/lenokp21/projeto-glass-html5/gh-pages/_imagens/glass-oculos-preto-peq.png";
+var especificacoes = "https://raw.githubusercontent.com/lenokp21/projeto-glass-html5/master/_imagens/especificacoes.png";
+var fotos = "https://raw.githubusercontent.com/lenokp21/projeto-glass-html5/master/_imagens/fotos.png";
+var multimidia = "https://raw.githubusercontent.com/lenokp21/projeto-glass-html5/master/_imagens/multimidia.png";
+var contato = "https://raw.githubusercontent.com/lenokp21/projeto-glass-html5/master/_imagens/contato.png";
+
+function mudaFoto(foto){
+	document.getElementById("icone").src = foto;
+}
